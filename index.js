49e2b3caedeeb7d5a3c07e67b7709d43cf20bfd0
samidarko <samidarko@gmail.com>
2016-11-17 00:27:33 +0800

    Redis storage implementation

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
  */
 const fs = require('fs');
 const events = require('events');
+const redis = require("redis"),
+    client = redis.createClient();
 
 /**
  * Events names
@@ -12,6 +14,9 @@ const events = require('events');
  */
 const bidEvent = 'bidEventName', auctionEndEvent = 'auctionEndEvent';
 
+client.on("error", function (err) {
+    console.log("Error connecting Redis" + err);
+});
 
 /**
  * Set a bidder and emit an event for a give interval
@@ -26,7 +31,7 @@ function setBidder(name, interval, ee) {
     console.log('create bidder', name, interval);
     const i = setInterval(() => {
         ee.emit(bidEvent, name)
-    }, 1000*interval);
+    }, 1000 * interval);
     ee.on(auctionEndEvent, () => clearInterval(i));
 }
 
@@ -36,39 +41,57 @@ function setBidder(name, interval, ee) {
  * @param startTime
  * @param auctionTime
  * @param increaseTime
+ * @param rc
  * @constructor
  */
-function Auction(ee, startTime, auctionTime, increaseTime) {
+function Auction(ee, startTime, auctionTime, increaseTime, rc) {
     this._ee = ee;
     this._startTime = startTime;
     this._auctionTime = auctionTime;
     this._increaseTime = increaseTime;
-    this._maxBid = 0;
-    this._lastBidder = '';
+    this._maxBidKey = 'maxBid';
+    this._lastBidderKey = 'lastBidder';
     this._interval = undefined;
+    this._rc = rc;
 
     this.setLastBidder = (name) => {
-        this._lastBidder = name;
-        this._maxBid += 0.1;
-        // TODO set redis
-        console.log(this._lastBidder, this._maxBid);
-        clearTimeout(this._interval);
-        const spentTime = (new Date().getTime() - this._startTime);
-        this._auctionTime += this._increaseTime;
-        this._interval = setTimeout(this._callBack, this._auctionTime - spentTime);
+        this._rc.set(this._lastBidderKey, name);
+        this._rc.get(this._maxBidKey, (err, value) => {
+            if (err) {
+                console.log('error in setLastBidder', err)
+            }
+            this._rc.set(this._maxBidKey, parseFloat(value) + 0.1);
+            console.log(name, parseFloat(value) + 0.1);
+            clearTimeout(this._interval);
+            const spentTime = (new Date().getTime() - this._startTime);
+            this._auctionTime += this._increaseTime;
+            this._interval = setTimeout(this._callBack, this._auctionTime - spentTime);
+        });
     };
 
     this.start = () => {
+        this._rc.set(this._maxBidKey, 0);
+        this._rc.set(this._lastBidderKey, '');
         this._interval = setTimeout(this._callBack, this._auctionTime);
     };
 
     this._callBack = () => {
         // on auction end event we stop
-        this._ee.emit(auctionEndEvent);
-        // TODO get from redis
-        console.log('Final Price', this._maxBid);
-        console.log('Winner', this._lastBidder);
-        console.log('total time', (new Date().getTime() - this._startTime)/1000, 'in seconds')
+        this._rc.get(this._maxBidKey, (err, total) => {
+            if (err) {
+                console.log('error in _callBack getting', this._maxBidKey, err)
+            }
+            this._rc.get(this._lastBidderKey, (err, lastBidder) => {
+                if (err) {
+                    console.log('error in _callBack getting', this._lastBidderKey, err)
+                }
+                this._ee.emit(auctionEndEvent);
+                this._rc.end(true);
+                console.log('Final Price', total);
+                console.log('Winner', lastBidder);
+                console.log('total time', (new Date().getTime() - this._startTime) / 1000, 'in seconds')
+            });
+        });
     }
 
 }
@@ -81,7 +104,7 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
     }
 
     const eventEmitter = new events.EventEmitter();
-    const auction = new Auction(eventEmitter, new Date().getTime(), 60000, 5000);
+    const auction = new Auction(eventEmitter, new Date().getTime(), 60000, 5000, client);
 
     eventEmitter.on(bidEvent, (name) => {
         auction.setLastBidder(name);
